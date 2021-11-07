12bb083227a58dd09ea60e741c338c7819c8f9ee
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-07 15:10:09 +0200

    assignment 2.4 update

--- 2.4-date/2.4-date.js
+++ 2.4-date/2.4-date.js
@@ -3,7 +3,7 @@ const tellDate = () => {
     const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
     const now = new Date();
     const currDate = now.toLocaleDateString("en-US", options);
-     
+
     const currDateParts = currDate.split(' ').map( val => {
         if (val.includes(',')) {
             return val.replace(',', '');
@@ -11,7 +11,12 @@ const tellDate = () => {
         return val;
     });
 
-    console.log(`Today is ${currDateParts[0]} the ${currDateParts[2]} of ${currDateParts[1]}, ${currDateParts[3]}`);
+    const weekday = currDateParts[0];
+    const day = currDateParts[2];
+    const month = currDateParts[1];
+    const year = currDateParts[3];
+
+    console.log(`Today is ${weekday} the ${day} of ${month}, ${year}`);
 }
 
 tellDate();
