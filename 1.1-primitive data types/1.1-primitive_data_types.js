6afd0308010110835ce1d44457902ec3282b87a0
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-07 12:07:38 +0200

    assignment 1.1 done

---
+++ 1.1-primitive data types/1.1-primitive_data_types.js
@@ -0,0 +1,10 @@
+const str = 'wheee';
+const num = 1;
+const obj = { };
+const t = true;
+const n = null;
+const ud = undefined;
+const symb = '@';
+
+
+console.log(typeof(str), typeof(num), typeof(obj), typeof(t), typeof(n), typeof(ud), typeof(symb));
