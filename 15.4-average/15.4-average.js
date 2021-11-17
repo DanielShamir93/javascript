89e053208d229bd06e9f8204a004d603e7f624e5
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-17 14:45:39 +0200

    assignment 15.4 done

---
+++ 15.4-average/15.4-average.js
@@ -0,0 +1,21 @@
+// 1
+// line 2: sum wasn't initialized
+// line 6: calcAverage returned the sum and not the average
+
+// 2
+// vscode
+
+// 3
+// self explained
+
+// 4
+
+function calcAverage(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / 3;
+}
+
+console.log(calcAverage([85, 90, 92]));
