1d1bb84ec8081120911b4cc4a68707569048e6c8
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-17 14:39:46 +0200

    assignment 15.3 done

---
+++ 15.3-sum_of_even/15.3-sum_of_even.js
@@ -0,0 +1,17 @@
+// 1
+// line 2: exceeded cell number
+
+// 2
+// vscode
+
+// 3
+// self explained
+
+//4
+
+function getSumOfEven(arr) {
+    return arr[0] + arr[2] + arr[4] + arr[6] + arr[8];
+}
+
+
+console.log(getSumOfEven([1,2,3,4,5,6,7,8,9,10]));
