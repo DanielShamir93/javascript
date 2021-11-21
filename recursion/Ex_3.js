641ac40c983211783413fefdd1837084593a923c
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-21 12:29:46 +0200

    recursion Ex3 done

---
+++ recursion/Ex_3.js
@@ -0,0 +1,38 @@
+const arr = Array.from('gnirts desrever');
+
+
+const reverseRecursion = (arr, left, right) => {
+    if (left > right) {
+        return arr.join('');
+    }
+
+    const tmp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = tmp;
+
+    return reverseRecursion(arr, left + 1, right - 1);
+}
+
+console.log(reverseRecursion(arr, 0, arr.length - 1));
+
+
+
+const reverseJS = (arr) => {
+    return arr.reverse().join('');
+}
+
+// console.log(reverseJS(arr));
+
+
+const reverseLoop = (arr) => {
+    let n = arr.length - 1;
+    for (let i = 0; i < n; i ++) {
+        let tmp = arr[i];
+        arr[i] = arr[n];
+        arr[n] = tmp;
+        n--;
+    }
+    return arr.join('');
+}
+
+// console.log(reverseLoop(arr));
