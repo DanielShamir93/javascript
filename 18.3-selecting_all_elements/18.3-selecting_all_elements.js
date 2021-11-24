300864efe49ef7f4a4c1b1f9f0b40c592a437582
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-24 14:35:29 +0200

    assignment 19.1 done

---
+++ 18.3-selecting_all_elements/18.3-selecting_all_elements.js
@@ -0,0 +1,6 @@
+const divs = document.querySelectorAll("div");
+const myFirstDiv = divs[0];
+const mySecondDiv = divs[1];
+const myThirdDiv = divs[2];
+const myFourthDiv = divs[3];
+console.log(mySecondDiv);
