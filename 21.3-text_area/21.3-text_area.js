9f9abeef24d6038bbb5fe6347652ce5e82c34857
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-29 18:21:37 +0200

    update assignment 21.3

--- 21.3-text_area/21.3-text_area.js
+++ 21.3-text_area/21.3-text_area.js
@@ -1,8 +1,16 @@
 document.querySelector('.submit').addEventListener('click', (e) => {
     const textarea = document.querySelector('textarea');
+    const h5 = document.querySelector('h5');
 
-    if (textarea.value.trim().length < 100) {
-        // text length is less than 100 characters
-        alert('Must have at least 100 characters');
+    if (textLength(textarea.value) < 100) {
+        h5.style.color = 'red';
+        h5.textContent = `Must contain at least 100 characters`;
+    } else {
+        h5.style.color = 'green';
+        h5.textContent = `Number of characters: ${textLength(textarea.value)}`;
     }
-})
+});
+
+const textLength = (str) => {
+    return str.match(/[\S]+/g).join('').length;
+}
