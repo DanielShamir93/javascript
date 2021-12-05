d9cff719e3c1b39e226819e36eebf6a71d4b2b3b
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-12-05 18:08:41 +0200

    assingment 24.2 done

---
+++ 24.2-pokeman/24.2-pokeman.js
@@ -0,0 +1,29 @@
+function Pokemon(pokemonName, pokemonType, pokemonAttackList) {
+  this.name = pokemonName;
+  this.type = pokemonType;
+  this.attackList = pokemonAttackList;
+}
+
+
+// const pokemon1 = new Pokemon('Matti', 'water', ['micAllaysOpen']);
+// const pokemon2 = new Pokemon('Ernest', 'fire', ['forkUsing']);
+// const pokemon3 = new Pokemon('Fida', 'wind', ['feed']);
+const pokemonArray = [  new Pokemon('Matti', 'water', ['mic allays open']),
+                        new Pokemon('Ernest', 'fire', ['fork using']),
+                        new Pokemon('Fida', 'wind', ['feed all'])];
+
+function callPokemon() {
+    console.log(`I choose you ${this.name}`);
+}
+
+function attack(attackNumber) {
+    console.log(`${this.name} used ${this.attackList[attackNumber]}`);
+}
+
+Pokemon.prototype.callPokemon = callPokemon;
+Pokemon.prototype.attack = attack;
+
+pokemonArray.forEach((pokemon) => {
+    pokemon.callPokemon();
+    pokemon.attack(0);
+});
